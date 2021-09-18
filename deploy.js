36869996ace0381de2cd4488643073125c15563c
kyometori <ac0xrpfs001@vivladi.net>
2021-09-19 03:55:30 +0800

    create a custom deploy script

--- deploy.js
+++ deploy.js
@@ -4,13 +4,14 @@
  * 每當指令的 metadata 有更新時，就應該執行一次此檔案
  * 注意全域指令有一小時 cooldown
  */
-
 const { Client } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
 const client = new Client({
   intents: ['GUILDS']
 });
+
+const GuildId = process.argv[2];
 
 const commands = [];
 
@@ -24,8 +25,13 @@ client.once('ready', async () => {
   console.log('開始部署');
 
   try {
-    client.guilds.cache.get('584395052856639488').commands.set(commands);
-    // client.application.commands.set(commands);
+    if (!GuildId) {
+      client.application.commands.set(commands);
+    } else {
+      const guild = client.guilds.cache.get(GuildId);
+      if (!guild) return console.log('無此伺服器');
+      guild.commands.set(commands);
+    }
   } catch (err) {
     console.log('出現錯誤');
     throw err;
@@ -33,6 +39,7 @@ client.once('ready', async () => {
 
   console.log('完成部署');
   console.log('程式可以結束');
+  if (!GuildId) console('你部署的是全域指令，請注意需等待一小時才會顯示於 Discord 上');
   console.log('------------------------------------')
 });
 
